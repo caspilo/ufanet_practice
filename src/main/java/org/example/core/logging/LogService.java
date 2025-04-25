75768915167285b3855768c5a8934ba9afff32cb
Baioret <ranreo.reo@gmail.com>
2025-04-25 09:37:43 +0500

    Логи вынесены в отдельную папку, добавлена текущая дата в название файла логов

--- src/main/java/org/example/core/logging/LogService.java
+++ src/main/java/org/example/core/logging/LogService.java
@@ -2,17 +2,24 @@ package org.example.core.logging;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.FileHandler;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class LogService {
     public static Logger logger = null;
     static {
-        try (FileInputStream ins = new FileInputStream("src/main/java/org/example/logging.properties")) {
-            LogManager.getLogManager().readConfiguration(ins);
+        try {
             logger = Logger.getLogger(LogService.class.getName());
+            String today = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+            FileHandler fh = new FileHandler("logs/log-" + today + ".txt", 1024 * 1024, 5, true);
+            fh.setFormatter(new SimpleFormatter());
+            logger.addHandler(fh);
             //logger.setUseParentHandlers(false);
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.err.println("Could not setup logger configuration: " + e);
         }
     }
