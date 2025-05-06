01aeb2197f758b8ab2654ac991683dc714f668e9
Baioret <ranreo.reo@gmail.com>
2025-05-06 09:46:23 +0500

    Random runtime in schedulable tasks

--- src/main/java/org/example/core/schedulable/DoSomething.java
+++ src/main/java/org/example/core/schedulable/DoSomething.java
@@ -1,13 +1,19 @@
 package org.example.core.schedulable;
 
 import java.util.Map;
+import java.util.Random;
 
 public class DoSomething implements Schedulable {
 
+    private final Random random = new Random();
+
     @Override
     public boolean execute(Map<String,String> params) {
+
+        int ex_time = random.nextInt(5000) + 1000;
+
         try {
-            Thread.sleep(1000); // имитация процесса выполнения
+            Thread.sleep(ex_time); // имитация процесса выполнения
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/org/example/core/schedulable/PushNotification.java
+++ src/main/java/org/example/core/schedulable/PushNotification.java
@@ -1,14 +1,20 @@
 package org.example.core.schedulable;
 
 import java.util.Map;
+import java.util.Random;
 
 public class PushNotification implements Schedulable {
 
+    private final Random random = new Random();
+
     @Override
     public boolean execute(Map<String, String> params) {
+
+        int ex_time = random.nextInt(5000) + 1000;
+
         if (params.containsKey("ID")) {
             try {
-                Thread.sleep(2000); // имитация процесса выполнения
+                Thread.sleep(ex_time); // имитация процесса выполнения
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
